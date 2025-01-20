276abb40143fa4b2743d654d8f6ac35f175ee243
QuocHung <hungnguyen@maple.com.vn>
2025-01-20 09:58:58 +0700

    Add project files.

---
+++ ElderlyCarePlatform.Domain/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElderlyCarePlatform.Domain.Entities
+{
+    public class User
+    {
+        public string? FullName { get; set; }
+        public string? Preferences { get; set; }
+        public DateTime DateOfBirth { get; set; } = DateTime.Now;
+    }
+}
---
+++ ElderlyCarePlatform.Insfrastructure/Data/ElderlyCareDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElderlyCarePlatform.Insfrastructure.Data
+{
+    public class ElderlyCareDbContext : DbContext
+    {
+        public ElderlyCareDbContext()
+        {
+            
+        }
+
+        public ElderlyCareDbContext(DbContextOptions<ElderlyCareDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
