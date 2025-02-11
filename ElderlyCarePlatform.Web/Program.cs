43119027fd8994d54c850ee1b0a685b7dd6004a2
ArchieAsano <ArchieAsano@users.noreply.github.com>
2025-02-12 06:28:06 +0700

    Add_Identity

--- ElderlyCarePlatform.Web/Program.cs
+++ ElderlyCarePlatform.Web/Program.cs
@@ -1,7 +1,23 @@
+using ElderlyCarePlatform.Domain.Entities;
+using ElderlyCarePlatform.Insfrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddIdentity<User, IdentityRole>(
+    options=>
+    {
+        options.User.RequireUniqueEmail = true;
+    }
+    ).AddEntityFrameworkStores<ElderlyCareDbContext>()
+    .AddTokenProvider<DataProtectorTokenProvider<User>>(TokenOptions.DefaultProvider);
+builder.Services.AddDbContext<ElderlyCareDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 var app = builder.Build();
 
